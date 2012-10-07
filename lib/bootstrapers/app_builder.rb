0ec3a88dc27cf69b262e33bf163676d36c9b10bb
xdite <xdite@rocodev.com>
2012-10-08 05:50:16 +0800

    add setup_development_environment

---
+++ lib/bootstrapers/app_builder.rb
@@ -0,0 +1,24 @@
+module Bootstrapers
+  class AppBuilder < Rails::AppBuilder
+
+    include Bootstrapers::Actions
+
+    def readme
+      template 'README.md.erb', 'README.md'
+    end
+
+    def remove_public_index
+      remove_file 'public/index.html'
+    end
+
+    def remove_rails_logo_image
+      remove_file 'app/assets/images/rails.png'
+    end
+
+    def raise_delivery_errors
+      replace_in_file 'config/environments/development.rb',
+        'raise_delivery_errors = false', 'raise_delivery_errors = true'
+    end
+
+  end
+end
--- lib/bootstrapers/generators/app_generator.rb
+++ lib/bootstrapers/generators/app_generator.rb
@@ -4,14 +4,32 @@ require 'rails/generators/rails/app/app_generator'
 
 module Bootstrapers
   class AppGenerator < Rails::Generators::AppGenerator
-    
+
     def finish_template
       invoke :bootstrapers_customization
       super
     end
 
     def bootstrapers_customization
+      invoke :remove_files_we_dont_need
+      invoke :setup_development_environment
     end
-    
+
+    def remove_files_we_dont_need
+      build :remove_public_index
+      build :remove_rails_logo_image
+    end
+
+    def setup_development_environment
+      say 'Setting up the development environment'
+      build :raise_delivery_errors
+    end
+
+    protected
+
+    def get_builder_class
+      Bootstrapers::AppBuilder
+    end
+
   end
 end
