7eae3cb8e583603057cd7b5bebe15d2901c67a8c
xdite <xdite@rocodev.com>
2012-12-21 21:30:40 +0800

    改用 diff

--- templates/capistrano/deploy/assets.rb
+++ templates/capistrano/deploy/assets.rb
@@ -19,7 +19,7 @@ namespace :deploy do
     DESC
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+      if capture("cd #{latest_release} && #{source.local.diff(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
