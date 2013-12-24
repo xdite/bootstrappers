fad1e8a8c41f0026b409bf2768fcbda36065d76e
xdite <xuite.joke@gmail.com>
2013-12-25 04:39:20 +0800

    remove jquery-ui

--- lib/bootstrappers/version.rb
+++ lib/bootstrappers/version.rb
@@ -1,3 +1,3 @@
 module Bootstrappers
-  VERSION = "4.2.0.9"
+  VERSION = "4.2.0.10"
 end
--- templates/javascripts/application.js
+++ templates/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require_self
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
 //= require twitter/bootstrap/alert
 //= require twitter/bootstrap/dropdown
